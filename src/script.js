895a941ccef7a1349941ce20a83e22068e55cfbb
emoltz <es5541@nyu.edu>
2022-06-03 16:13:11 -0400

    functions added

--- src/script.js
+++ src/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+//
+const score0_el = document.getElementById('score--0');
+const score1_el = document.getElementById('score--1');
+let player1_score = document.getElementById('current--0');
+let player2_score = document.getElementById('current--1');
+
+function hideDice(){
+
+}
+
+function init(){
+//set scores to 0
+    score0_el.textContent = String(0);
+    score1_el.textContent = String(0);
+}
+init();
+
