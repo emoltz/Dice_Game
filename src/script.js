96bca07c6e3975e353bf750c882f8f984103fa0b
emoltz <es5541@nyu.edu>
2022-06-04 12:14:57 -0400

    new game event listener

--- src/script.js
+++ src/script.js
@@ -1,10 +1,13 @@
 'use strict';
 
 //
-const score0_el = document.getElementById('score--0');
-const score1_el = document.getElementById('score--1');
-let player1_score = document.getElementById('current--0');
-let player2_score = document.getElementById('current--1');
+const player1TotalScore_el = document.getElementById('score--0');
+const player2TotalScore_el = document.getElementById('score--1');
+let current_player1 = document.getElementById('current--0');
+let current_player2 = document.getElementById('current--1');
+const newGameButton = document.querySelector('.btn--new');
+
+
 
 function hideDice(){
 
@@ -12,8 +15,11 @@ function hideDice(){
 
 function init(){
 //set scores to 0
-    score0_el.textContent = String(0);
-    score1_el.textContent = String(0);
+    player1TotalScore_el.textContent = String(0);
+    player2TotalScore_el.textContent = String(0);
 }
 init();
 
+
+//listeners
+newGameButton.addEventListener('click',init);
