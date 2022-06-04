1930e4bd72cdcc64be9d0e50f69761293b903b61
emoltz <es5541@nyu.edu>
2022-06-04 17:21:40 -0400

    ability to change name (commented out for now)

--- src/script.js
+++ src/script.js
@@ -1,15 +1,26 @@
 'use strict';
 
-//
+//VARS
 const player1TotalScore_el = document.getElementById('score--0');
 const player2TotalScore_el = document.getElementById('score--1');
-let current_player1 = document.getElementById('current--0');
-let current_player2 = document.getElementById('current--1');
+let currentScore_player1 = document.getElementById('current--0');
+let currentScore_player2 = document.getElementById('current--1');
 const newGameButton = document.querySelector('.btn--new');
+let player1Name = document.getElementById('name--0');
+let player2Name = document.getElementById('name--1');
 
+function changeName(name, player1){
+    if(player1 === true){
+        player1Name.textContent = name;
+    }
+    else{
+        player2Name.textContent = name;
+    }
 
+}
 
-function hideDice(){
+function hidePigs(){
+
 
 }
 
@@ -17,7 +28,18 @@ function init(){
 //set scores to 0
     player1TotalScore_el.textContent = String(0);
     player2TotalScore_el.textContent = String(0);
+    //// this changes player names:
+    // let nameTemp = window.prompt("Enter Player1 Name", "Your Name")
+    // changeName(nameTemp, true)
+    // nameTemp = window.prompt("Enter Player2 Name", "Your Name");
+    // changeName(nameTemp, false);
 }
+
+
+
+
+//MAIN:
+
 init();
 
 
