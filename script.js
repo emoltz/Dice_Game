34da4f92bdbea1792e83890871c3812ab541662f
emoltz <es5541@nyu.edu>
2022-06-10 11:28:53 -0400

    pig logic incoming...

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
 'use strict';
+console.log("Github link: " + "https://github.com/emoltz/Pass-The-Pigs");
 
 const winningScore = 100;
 let playing = true;
-//______________PIG LOGIC
-/*
-
- */
-
-
-
-
-
 
 
 
@@ -45,8 +37,12 @@ const diceElement = document.querySelector('.pigs');
 
 
 //______________Roll
+function changePlayersCurrentScore(){
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+}
+
 function rollDice() {
-    if(playing){
+    if (playing) {
         // generate random roll
         let dice = Math.trunc(Math.random() * 6 + 1);
 
@@ -58,10 +54,9 @@ function rollDice() {
         // check for rolled 1 -- if true, switch to next player
         if (dice !== 1) {
             currentScore += dice;
-            document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-
+            // document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+            changePlayersCurrentScore()
         } else {
-
             switchActivePlayer();
         }
     }
@@ -69,6 +64,60 @@ function rollDice() {
 
 }
 
+//______________PIG LOGIC
+
+//TODO turn into object
+var lookup = {
+    1: {range:[0,.349], points: null, name: "side (no dot)"},
+    2: {range: [.3491,.6511], points: null, name: "side (dot)"},
+    3: {range: [.6522, .8751], points: 5, name: "Razorback"},
+    4: {range: [.8752, .9631], points: 5, name: "Trotter"},
+    5: {range: [.9632, .9932], points: 10, name: "Snouter"},
+    6: {range: [.9933, .9960], points: 15, name: "Leaning Jowler"},
+    7: {range: [.9961, 1], points: 0, name: "Oinker!"}
+}
+
+
+function rollPigs(){
+    // let min = 1;
+    // let max = 7;
+    // let roll1 = Math.round(max / (Math.random() * max + min));
+    // let roll2 = Math.round(max / (Math.random() * max + min));
+    let roll1 = (Math.random());
+    let roll2 = (Math.random());
+
+
+    console.log("Pig Roll 1: " + roll1);
+    console.log("Pig Roll 2: " + roll2);
+
+
+    // switch (roll1){
+    //     case 1: //no dot
+    //
+    //         break;
+    //     case 2: //dot
+    //
+    //         break;
+    //     case 3: //back
+    //         break;
+    //     case 4: // Standing
+    //         break;
+    //     case 5: // snout
+    //         break;
+    //     case 6: // ear
+    //         break;
+    //     case 7: //touching the other pig
+    //         currentScore = 0;
+    //         changePlayersCurrentScore();
+    //         totalScores[activePlayer] = 0;
+    //         hold();
+    //         break;
+    //     default:
+    //         console.log("Pig roll error -- Math");
+    //         break;
+    // }
+}
+rollPigs();
 //______________FUNCTIONS
 
 function switchActivePlayer() {
@@ -135,14 +184,13 @@ function hold() {
     //add current score to score of active player
     totalScores[activePlayer] += currentScore;
     console.log(totalScores[activePlayer])
-    if(activePlayer===0){
+    if (activePlayer === 0) {
         player1TotalScore_el.textContent = totalScores[activePlayer]
-    }
-    else{
+    } else {
         player2TotalScore_el.textContent = totalScores[activePlayer];
     }
     // check if players score is over 100
-    if(totalScores[activePlayer] >= winningScore){
+    if (totalScores[activePlayer] >= winningScore) {
         document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
         document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
         playing = false;
---
+++ test.js
@@ -0,0 +1,10 @@
+var lookup = {
+    1: {range:[0,.349], points: null, name: "side (no dot)"},
+    2: {range: [.3491,.6511], points: null, name: "side (dot)"},
+    3: {range: [.6522, .8751], points: 5, name: "Razorback"},
+    4: {range: [.8752, .9631], points: 5, name: "Trotter"},
+    5: {range: [.9632, .9932], points: 10, name: "Snouter"},
+    6: {range: [.9933, .9960], points: 15, name: "Leaning Jowler"},
+    7: {range: [.9961, 1], points: 0, name: "Oinker!"}
+}
+console.log(lookup.valueOf())
