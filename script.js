239b697c9a53897611114a64f6563d7c4fa67c46
emoltz <es5541@nyu.edu>
2022-06-12 10:27:25 -0400

    fixed issue

--- script.js
+++ script.js
@@ -87,40 +87,42 @@ function rollPigs(){
     let sideCalc = Math.trunc(Math.random() *2 + 1);
     let roll1PigDot = false;
     let roll2PigDot = false;
+    if(playing){
+        if (sideCalc == 1){
+            roll1PigDot = true;
+            // console.log("Pig 1: dot!");
+        }
+        else{
+            // console.log("Pig 1: no dot!");
+        }
+        sideCalc = Math.trunc(Math.random() *2 + 1);
+        if (sideCalc == 1){
+            roll2PigDot = true;
+            // console.log("Pig 2: dot!");
+        }
+        else{
+            // console.log("Pig 2: no dot!");
+        }
 
-    if (sideCalc == 1){
-        roll1PigDot = true;
-        // console.log("Pig 1: dot!");
-    }
-    else{
-        // console.log("Pig 1: no dot!");
-    }
-    sideCalc = Math.trunc(Math.random() *2 + 1);
-    if (sideCalc == 1){
-        roll2PigDot = true;
-        // console.log("Pig 2: dot!");
-    }
-    else{
-        // console.log("Pig 2: no dot!");
+        //check for Pig Out
+        let pigOut = false;
+        if ((roll1PigDot == false && roll2PigDot == true) || (roll1PigDot == true && roll2PigDot == false)){
+            currentScore = 0;
+            changePlayersCurrentScore();
+            switchActivePlayer();
+            console.log("Pig out!");
+            pigOut = true;
+        }
+
+        if (!pigOut){
+            pigRoll(roll1,roll1PigDot, pig1Element);
+            pigRoll(roll2,roll2PigDot, pig2Element);
+        }
+        else{
+            pigOut = false;
+        }
     }
 
-    //check for Pig Out
-    let pigOut = false;
-    if ((roll1PigDot == false && roll2PigDot == true) || (roll1PigDot == true && roll2PigDot == false)){
-        currentScore = 0;
-        changePlayersCurrentScore();
-        switchActivePlayer();
-        console.log("Pig out!");
-        pigOut = true;
-    }
-
-    if (!pigOut){
-        pigRoll(roll1,roll1PigDot, pig1Element);
-        pigRoll(roll2,roll2PigDot, pig2Element);
-    }
-    else{
-        pigOut = false;
-    }
 
 
 }
@@ -230,6 +232,8 @@ function resetScores() {
     currentScore_player2_element.textContent = String(currentScore);
     player1TotalScore_el.textContent = String(currentScore);
     player2TotalScore_el.textContent = String(currentScore);
+    totalScores[0] = 0;
+    totalScores[1] = 0;
 }
 
 function setPlayerNames(name1, name2) {
@@ -251,7 +255,8 @@ function init() {
     player1_el.classList.remove('player--winner');
     player2_el.classList.remove('player--winner');
     player1_el.classList.add('player--active');
-    player2_el.classList.remove('player--active')
+    player2_el.classList.remove('player--active');
+    playing = true;
 
 }
 
@@ -289,135 +294,3 @@ holdButton.addEventListener('click', hold);
 //______________MAIN:
 
 init();
-
-//confetti:
-var maxParticleCount = 150; //set max confetti count
-var particleSpeed = 2; //set the particle animation speed
-var startConfetti; //call to start confetti animation
-var stopConfetti; //call to stop adding confetti
-var toggleConfetti; //call to start or stop the confetti animation depending on whether it's already running
-var removeConfetti; //call to stop the confetti animation and remove all confetti immediately
-
-(function() {
-    startConfetti = startConfettiInner;
-    stopConfetti = stopConfettiInner;
-    toggleConfetti = toggleConfettiInner;
-    removeConfetti = removeConfettiInner;
-    var colors = ["DodgerBlue", "OliveDrab", "Gold", "Pink", "SlateBlue", "LightBlue", "Violet", "PaleGreen", "SteelBlue", "SandyBrown", "Chocolate", "Crimson"]
-    var streamingConfetti = false;
-    var animationTimer = null;
-    var particles = [];
-    var waveAngle = 0;
-
-    function resetParticle(particle, width, height) {
-        particle.color = colors[(Math.random() * colors.length) | 0];
-        particle.x = Math.random() * width;
-        particle.y = Math.random() * height - height;
-        particle.diameter = Math.random() * 10 + 5;
-        particle.tilt = Math.random() * 10 - 10;
-        particle.tiltAngleIncrement = Math.random() * 0.07 + 0.05;
-        particle.tiltAngle = 0;
-        return particle;
-    }
-
-    function startConfettiInner() {
-        var width = window.innerWidth;
-        var height = window.innerHeight;
-        window.requestAnimFrame = (function() {
-            return window.requestAnimationFrame ||
-                window.webkitRequestAnimationFrame ||
-                window.mozRequestAnimationFrame ||
-                window.oRequestAnimationFrame ||
-                window.msRequestAnimationFrame ||
-                function (callback) {
-                    return window.setTimeout(callback, 16.6666667);
-                };
-        })();
-        var canvas = document.getElementById("confetti-canvas");
-        if (canvas === null) {
-            canvas = document.createElement("canvas");
-            canvas.setAttribute("id", "confetti-canvas");
-            canvas.setAttribute("style", "display:block;z-index:999999;pointer-events:none");
-            document.body.appendChild(canvas);
-            canvas.width = width;
-            canvas.height = height;
-            window.addEventListener("resize", function() {
-                canvas.width = window.innerWidth;
-                canvas.height = window.innerHeight;
-            }, true);
-        }
-        var context = canvas.getContext("2d");
-        while (particles.length < maxParticleCount)
-            particles.push(resetParticle({}, width, height));
-        streamingConfetti = true;
-        if (animationTimer === null) {
-            (function runAnimation() {
-                context.clearRect(0, 0, window.innerWidth, window.innerHeight);
-                if (particles.length === 0)
-                    animationTimer = null;
-                else {
-                    updateParticles();
-                    drawParticles(context);
-                    animationTimer = requestAnimFrame(runAnimation);
-                }
-            })();
-        }
-    }
-
-    function stopConfettiInner() {
-        streamingConfetti = false;
-    }
-
-    function removeConfettiInner() {
-        stopConfetti();
-        particles = [];
-    }
-
-    function toggleConfettiInner() {
-        if (streamingConfetti)
-            stopConfettiInner();
-        else
-            startConfettiInner();
-    }
-
-    function drawParticles(context) {
-        var particle;
-        var x;
-        for (var i = 0; i < particles.length; i++) {
-            particle = particles[i];
-            context.beginPath();
-            context.lineWidth = particle.diameter;
-            context.strokeStyle = particle.color;
-            x = particle.x + particle.tilt;
-            context.moveTo(x + particle.diameter / 2, particle.y);
-            context.lineTo(x, particle.y + particle.tilt + particle.diameter / 2);
-            context.stroke();
-        }
-    }
-
-    function updateParticles() {
-        var width = window.innerWidth;
-        var height = window.innerHeight;
-        var particle;
-        waveAngle += 0.01;
-        for (var i = 0; i < particles.length; i++) {
-            particle = particles[i];
-            if (!streamingConfetti && particle.y < -15)
-                particle.y = height + 100;
-            else {
-                particle.tiltAngle += particle.tiltAngleIncrement;
-                particle.x += Math.sin(waveAngle);
-                particle.y += (Math.cos(waveAngle) + particle.diameter + particleSpeed) * 0.5;
-                particle.tilt = Math.sin(particle.tiltAngle) * 15;
-            }
-            if (particle.x > width + 20 || particle.x < -20 || particle.y > height) {
-                if (streamingConfetti && particles.length <= maxParticleCount)
-                    resetParticle(particle, width, height);
-                else {
-                    particles.splice(i, 1);
-                    i--;
-                }
-            }
-        }
-    }
-})();
